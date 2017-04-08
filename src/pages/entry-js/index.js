5f149edafc67683f1ddcc14652b7d1dca54cd99f
grARM <gricode@126.com>
2017-04-08 22:17:01 +0800

    ok

--- src/pages/entry-js/index.js
+++ src/pages/entry-js/index.js
@@ -1,5 +1,6 @@
 console.log('ok01')
 require('../../common/styles/common.css');
 require('../entry-style/index.less');
+require('../../modules/first-module/module.css');
 var common = require('../../common/scripts/common.js');
 var firstModules = require('../../modules/first-module/module.js');
