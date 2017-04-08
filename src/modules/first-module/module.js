9c9d4e5f9ea7bd1af8de70af24eb601db0553302
grARM <gricode@126.com>
2017-04-08 21:59:34 +0800

    first module

---
+++ src/modules/first-module/module.js
@@ -0,0 +1,4 @@
+console.log('first module');
+exports.render = function(){
+	console.log('first module render!');
+}
--- src/pages/entry-js/index.js
+++ src/pages/entry-js/index.js
@@ -2,3 +2,4 @@ console.log('ok01')
 require('../../common/styles/common.css');
 require('../entry-style/index.less');
 var common = require('../../common/scripts/common.js');
+var firstModules = require('../../modules/first-module/module.js');
